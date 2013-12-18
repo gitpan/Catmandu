c3775443b2c479cc2ae0f31963e3a82a9e994c08
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2013-12-18 13:48:57 +0000

    Import of NICS/Catmandu-0.8003 from CPAN.
    gitpan-cpan-distribution: Catmandu
    gitpan-cpan-version:      0.8003
    gitpan-cpan-path:         NICS/Catmandu-0.8003.tar.gz
    gitpan-cpan-author:       NICS
    gitpan-cpan-maturity:     released

--- lib/Catmandu.pm
+++ lib/Catmandu.pm
@@ -37,11 +37,11 @@ To install all Catmandu components in one easy step:
 
 =head1 VERSION
 
-Version 0.8002
+Version 0.8003
 
 =cut
 
-our $VERSION = '0.8002';
+our $VERSION = '0.8003';
 
 =head1 SYNOPSIS
 
